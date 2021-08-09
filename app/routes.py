3d6ef5a2b5d601264ca169d21ceb96f6985df6cc
Patrick <pat.stlouis91@gmail.com>
2021-08-08 22:43:24 -0400

    win-95 theme

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,18 @@
 from flask import current_app as app
+from flask import render_template
 
-@app.route('/')
+@app.route("/", methods=["GET"])
 def index():
-    return "hello world"
+    return render_template("pages/landing.jinja")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("pages/login.jinja")
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    return render_template("pages/dashboard.jinja")
+
+@app.route("/secret", methods=["GET"])
+def secret():
+    return render_template("pages/secret.jinja")
